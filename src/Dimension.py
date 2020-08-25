1cbd689a59884724503d2f595352d1a31cf864d4
fainc <farincorperated@gmail.com>
2020-08-25 15:18:25 -0700

    Changed Dimension vars to floats instead of int

--- src/Dimension.py
+++ src/Dimension.py
@@ -1,26 +1,26 @@
 class Dimension: 
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: float, height: float):
         super().__init__()
 
-        self.width: int = width
-        self.height: int = height
+        self.width: float = width
+        self.height: float = height
     
     @property
-    def width(self) -> int:
+    def width(self) -> float:
         return self.width
 
     @width.setter
-    def width(self, value: int):
+    def width(self, value: float):
         self._width = value
 
     @property
-    def height(self) -> int:
+    def height(self) -> float:
         return self.height
 
     @height.setter
-    def height(self, value: int):
+    def height(self, value: float):
         self._height = value
 
-    def calculateArea(self):
+    def calculateArea(self) -> float:
         return self.height * self.width
